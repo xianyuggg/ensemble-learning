8cbf6544c3c8d29edf9a52be953791181db7b5ff
XianyuGu <guxy17@mails.tsinghua.edu.cn>
2020-05-09 09:34:40 +0800

    feat: change some parameters

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from settings import EnsembleConfig
 
 # DTREE, SVM
 # BAGGING, ADA_BOOST_M1, SINGLE
-config = EnsembleConfig(bagging_times=50, ada_times=30, classifier_mode='DTREE', ensemble_mode='ADA_BOOST_M1', external_w2v=False, tf_idf=False)
+config = EnsembleConfig(bagging_times=100, ada_times=30, classifier_mode='DTREE', ensemble_mode='BAGGING', external_w2v=False, tf_idf=False)
 
 # raw_data can be found in raw_data/test.csv, train.csv...
-# divide_set(config)
+divide_set(config)
 
 # dataset path can be found in dataset/x_train.npy...
 x_train, y_train, x_validation, y_validation, x_test = load_data()
